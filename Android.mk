6e241d69800e264317ab6f58363679fc97393f13
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-03-21 01:59:41 +0800

    sdm660-common: Add C1N

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter DDV DRG PL2,$(TARGET_DEVICE)),)
+ifneq ($(filter DDV DRG PL2 C1N,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
